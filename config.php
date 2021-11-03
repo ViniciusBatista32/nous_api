abf17854467527423dd3ff3a92645aad77bd7f14
Vinicius Araujo <viniaraujo417@gmail.com>
2021-11-02 21:56:17 -0300

    Arrumando config.php Adicionando gitignore

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-const DOMAIN = "localhost";
+const DOMAIN = "allvinatto.com";
 const BASE_URL = DOMAIN . "/nous_api/";
 
 const DB_PORT = "3306";
-const DB_NAME = "nous";
-const DB_USER = "root";
-const DB_PASSWORD = "";
+const DB_NAME = "u516109380_nous";
+const DB_USER = "u516109380_nous";
+const DB_PASSWORD = "Tdah_6696";
